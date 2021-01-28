fe1c6e3bf19a78c64806f3c71ed9bd32b40dfbe9
Ahmadmansoury <Ahmadmansoury94@gmail.com>
2021-01-28 15:40:20 +0200

    lab 05 28/1/2021

--- app.js
+++ app.js
@@ -11,18 +11,18 @@ if(fromWhere=='napoli'){
     var order = prompt('what kind do you want marghrita or vegatables?');
     
 
-    while(order !=='margherita'&& !=='vegatables'){
+    while(order !=='marghrita'&&  order != 'vegatables'){
          
-        order =prompt('please write only margherita or vegatables ')
+        order =prompt('please write only marghrita or vegatables ')
     }
 
 
     var pizza = '';
 
     if(order === 'marghrita'){
-        pizza = '<img src=" images/ pizza-margherita.jpg" />';
+        pizza = '<img src="pizza-margherita.jpg" />';
     }else if (order === 'vegatabels'){
-        pizza = '<img src="images/ vegatables pizza.jpg" />'; 
+        pizza = '<img src="vegatables pizza.jpg" />'; 
     }
 
     var result ='';
@@ -32,10 +32,9 @@ if(fromWhere=='napoli'){
     var numPizza=prompt('how many pizza do you want');
 
     for(var i = 0 ; i<numPizza ; i ++){
-        result = result + pizza;
+        document.write(result);
     }
-
-
+    
 
 
 
