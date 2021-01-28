4f94827f805051c5c7a6593660810fd72c03f915
Ahmadmansoury <Ahmadmansoury94@gmail.com>
2021-01-28 14:35:06 +0200

    add lab 05

--- app.js
+++ app.js
@@ -8,5 +8,35 @@ if(fromWhere=='napoli'){
 }else if (fromWhere == 'lambardia'){
     message = 'Forza Rossoniri';}
     
+    var order = prompt('what kind do you want marghrita or vegatables?');
     
+
+    while(order !=='margherita'&& !=='vegatables'){
+         
+        order =prompt('please write only margherita or vegatables ')
+    }
+
+
+    var pizza = '';
+
+    if(order === 'marghrita'){
+        pizza = '<img src=" images/ pizza-margherita.jpg" />';
+    }else if (order === 'vegatabels'){
+        pizza = '<img src="images/ vegatables pizza.jpg" />'; 
+    }
+
+    var result ='';
+
+    result = pizza;
+
+    var numPizza=prompt('how many pizza do you want');
+
+    for(var i = 0 ; i<numPizza ; i ++){
+        result = result + pizza;
+    }
+
+
+
+
+
 document.write('<h3>' + fromWhere +'</h3>')
